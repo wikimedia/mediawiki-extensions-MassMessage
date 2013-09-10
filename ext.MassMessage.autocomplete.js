095a7fd54821a185677d4d0cc0d6f8310311fbef
Reedy <reedy@wikimedia.org>
2013-09-10 03:25:40 +0100

    Spaces to tabs
    Change-Id: Ib2e490414057aab06060b00dc7ac3080dea4fdd6

--- ext.MassMessage.autocomplete.js
+++ ext.MassMessage.autocomplete.js
@@ -4,23 +4,23 @@
  * and resources/mediawiki/mediawiki.searchSuggest.js
  */
 ( function ( mw, $ ) {
-    $( function () {
-        'use strict';
-        $( '#mw-massmessage-form-spamlist' ).autocomplete({
-            source: function( request, response ) {
-                var api = new mw.Api();
-                api.get({
-                    action: 'opensearch',
-                    search: request.term,
-                    suggest: ''
-                }).done( function (data) {
-                        response( data[1] );
-                    });
-            },
-            select: function (event, ui ) {
-                $( '#mw-massmessage-form-spamlist' ).val( ui.term );
-            }
-        });
-    } );
+	$( function () {
+		'use strict';
+		$( '#mw-massmessage-form-spamlist' ).autocomplete({
+			source: function( request, response ) {
+				var api = new mw.Api();
+				api.get({
+					action: 'opensearch',
+					search: request.term,
+					suggest: ''
+				}).done( function (data) {
+						response( data[1] );
+					});
+			},
+			select: function (event, ui ) {
+				$( '#mw-massmessage-form-spamlist' ).val( ui.term );
+			}
+		});
+	} );
 
 }( mediaWiki, jQuery ) );
--- ext.MassMessage.badhtml.js
+++ ext.MassMessage.badhtml.js
@@ -3,61 +3,61 @@
  * from http://www.raymondcamden.com/index.cfm/2012/1/23/Detecting-invalid-HTML-with-JavaScript
  */
 ( function ( mw, $ ) {
-    $( function () {
-        'use strict';
-        $( '#mw-massmessage-form-message' ).delayedBind( 500, 'keyup', function( ) {
-            var code, regex, matches, tags, possibles, tag;
-            code = $.trim( $( '#mw-massmessage-form-message' ).val() );
-            if( code === '' ) {
-                return;
-            }
+	$( function () {
+		'use strict';
+		$( '#mw-massmessage-form-message' ).delayedBind( 500, 'keyup', function( ) {
+			var code, regex, matches, tags, possibles, tag;
+			code = $.trim( $( '#mw-massmessage-form-message' ).val() );
+			if( code === '' ) {
+				return;
+			}
 
-            regex = /<.*?>/g;
-            matches = code.match(regex);
-            if( !matches.length ) {
-                return;
-            }
+			regex = /<.*?>/g;
+			matches = code.match(regex);
+			if( !matches.length ) {
+				return;
+			}
 
-            tags = {};
+			tags = {};
 
-            $.each(matches, function( idx, itm ) {
-                var realTag, tag;
-                //if the tag is, <..../>, it's self closing
-                if ( itm.substr( itm.length - 2, itm.length ) !== '/>' ) {
+			$.each(matches, function( idx, itm ) {
+				var realTag, tag;
+				//if the tag is, <..../>, it's self closing
+				if ( itm.substr( itm.length - 2, itm.length ) !== '/>' ) {
 
-                    //strip out any attributes
-                    tag = itm.replace(/[<>]/g, '').split(' ')[0];
-                    //start or end tag?
-                    if ( tag.charAt(0) !== '/' ) {
-                        if ( tags.hasOwnProperty( tag ) ) {
-                            tags[tag]++;
-                        } else {
-                            tags[tag] = 1;
-                        }
-                    } else {
-                        realTag = tag.substr(1, tag.length);
-                        if (tags.hasOwnProperty(realTag)) {
-                            tags[realTag]--;
-                        } else {
-                            tags[realTag] = -1;
-                        }
-                    }
-                }
-            });
+					//strip out any attributes
+					tag = itm.replace(/[<>]/g, '').split(' ')[0];
+					//start or end tag?
+					if ( tag.charAt(0) !== '/' ) {
+						if ( tags.hasOwnProperty( tag ) ) {
+							tags[tag]++;
+						} else {
+							tags[tag] = 1;
+						}
+					} else {
+						realTag = tag.substr(1, tag.length);
+						if (tags.hasOwnProperty(realTag)) {
+							tags[realTag]--;
+						} else {
+							tags[realTag] = -1;
+						}
+					}
+				}
+			});
 
-            possibles = [];
-            for ( tag in tags ) {
-                if ( tags[tag] !== 0 ) {
-                    possibles.push( '<' + tag + '>' );
-                }
-            }
-            if (possibles.length) {
-                mw.notify(
-                    mw.message( 'massmessage-badhtml', possibles.join(', '), possibles.length ).text(),
-                    { tag: 'massmessage-html-warning' }  // Show only one notification at a time
-                );
-            }
-        });
-    });
+			possibles = [];
+			for ( tag in tags ) {
+				if ( tags[tag] !== 0 ) {
+					possibles.push( '<' + tag + '>' );
+				}
+			}
+			if (possibles.length) {
+				mw.notify(
+					mw.message( 'massmessage-badhtml', possibles.join(', '), possibles.length ).text(),
+					{ tag: 'massmessage-html-warning' }  // Show only one notification at a time
+				);
+			}
+		});
+	});
 
 }( mediaWiki, jQuery ) );
