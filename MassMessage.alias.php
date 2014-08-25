64e61da5c8e8a54e7d66a7ab0df35b0bce84ef27
Kunal Mehta <legoktm@gmail.com>
2014-08-25 14:06:16 -0700

    Fix aliases for CreateMassMessageList and EditMassMessageList
    Change-Id: I4b3e152f9071eb12a89722cb1ea42ae516bfbda6

--- MassMessage.alias.php
+++ MassMessage.alias.php
@@ -12,8 +12,8 @@ $specialPageAliases = array();
 /** English (English) */
 $specialPageAliases['en'] = array(
 	'MassMessage' => array( 'MassMessage' ),
-	'CreateMassMessage' => array( 'CreateMassMessage' ),
-	'EditMassMessage' => array( 'EditMassMessage' ),
+	'CreateMassMessageList' => array( 'CreateMassMessageList' ),
+	'EditMassMessageList' => array( 'EditMassMessageList' ),
 );
 
 /** Arabic (العربية) */
