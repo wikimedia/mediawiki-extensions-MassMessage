186b2d4c62307f2bc2ec87e626f4541e2bc7ae8d
wctaiwan <wctaiwan@gmail.com>
2014-11-07 22:30:37 -0500

    Fix inaccurate comment and indentation
    Change-Id: Iedaebb9491b6700bd51cecbbcc4483612ea49be4

--- modules/ext.MassMessage.special.js
+++ modules/ext.MassMessage.special.js
@@ -22,27 +22,27 @@
 					'prop': 'info',
 					'indexpageids': true
 				} ).done( function ( data ) {
-						if ( data && data.query &&
-							// If the page exists and has a supported content model
-							( data.query.pageids[0] !== '-1' &&
-								data.query.pages[data.query.pageids[0]].contentmodel === 'wikitext' ||
-								data.query.pages[data.query.pageids[0]].contentmodel === 'MassMessageListContent' ) ||
-							// Or if the text refers to a category
-							data.query.pages[data.query.pageids[0]].ns === 14
-							) {
-							// No error message is displayed
-							$spamliststatus
-								.removeClass( 'invalid' )
-								.text( '' );
-						} else {
-							// Otherwise, display an error notice
-							$spamliststatus
-								.addClass( 'invalid' )
-								.text( mw.message( 'massmessage-parse-badpage', pagetitle ).text() );
-						}
-					} );
+					if ( data && data.query &&
+						// If the page exists and has a supported content model
+						( data.query.pageids[0] !== '-1' &&
+							data.query.pages[data.query.pageids[0]].contentmodel === 'wikitext' ||
+							data.query.pages[data.query.pageids[0]].contentmodel === 'MassMessageListContent' ) ||
+						// Or if the text refers to a category
+						data.query.pages[data.query.pageids[0]].ns === 14
+					) {
+						// No error message is displayed
+						$spamliststatus
+							.removeClass( 'invalid' )
+							.text( '' );
+					} else {
+						// Otherwise, display an error notice
+						$spamliststatus
+							.addClass( 'invalid' )
+							.text( mw.message( 'massmessage-parse-badpage', pagetitle ).text() );
+					}
+				} );
 			} else {
-				// If no text is entered or if the text refers to a category, don't display any warning
+				// If no text is entered, don't display any warning
 				$spamliststatus
 					.removeClass( 'invalid' )
 					.text( '' );
