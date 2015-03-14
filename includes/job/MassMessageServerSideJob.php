f02c7687fea114d0555deb32e69ded855f05ac3b
Kunal Mehta <legoktm@gmail.com>
2015-03-13 17:16:01 -0700

    Use WikiPage::doEditContent for ServerSideJob
    This will bypass permission checking and any checks like AbuseFilter
    
    Change-Id: Ic420f06c47c09bfdf1dc35125e6a63b54d925403

--- includes/job/MassMessageServerSideJob.php
+++ includes/job/MassMessageServerSideJob.php
@@ -31,4 +31,33 @@ class MassMessageServerSideJob extends MassMessageJob {
 	function makeText( $stripTildes = false ) {
 		return $this->params['message'];
 	}
+
+	protected function editPage() {
+		$user = MassMessage::getMessengerUser();
+		$page = WikiPage::factory( $this->title );
+		$subject = $this->params['subject'];
+		$text = "== $subject ==\n\n{$this->makeText()}";
+		$flags = 0;
+		if ( $page->exists() ) {
+			$oldContent = $page->getContent( Revision::RAW );
+			$text = $oldContent->getNativeData() . "\n\n" . $text;
+			$flags = $flags | EDIT_UPDATE;
+		} else {
+			$flags = $flags | EDIT_NEW;
+		}
+		if ( $this->title->inNamespace( NS_USER_TALK ) ) {
+			$flags = $flags | EDIT_FORCE_BOT;
+		}
+		$status = $page->doEditContent(
+			new WikitextContent( $text ),
+			$subject,
+			$flags,
+			false,
+			$user
+		);
+		if ( !$status->isOK() ) {
+			$errors = $status->getErrorsArray();
+			$this->logLocalFailure( $errors[0] );
+		}
+	}
 }
