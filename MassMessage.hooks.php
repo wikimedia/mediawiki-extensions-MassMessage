9c9b03408f39f1435e832f20b82521bcc4ffd5f8
Reedy <reedy@wikimedia.org>
2016-08-05 17:53:46 +0100

    Remove 'UnitTestList' hook
    No longer needed now that extension unittests are autodiscovered.
    
    Bug: T142120
    Bug: T142121
    Change-Id: Ibe1fc06518d56a2ddb3847f0e896a7f1975ae57b

--- MassMessage.hooks.php
+++ MassMessage.hooks.php
@@ -116,28 +116,6 @@ class MassMessageHooks {
 	}
 
 	/**
-	 * Load our unit tests
-	 */
-	public static function onUnitTestsList( &$files ) {
-		// @codeCoverageIgnoreStart
-		$directoryIterator = new RecursiveDirectoryIterator( __DIR__ . '/tests/' );
-
-		/**
-		 * @var SplFileInfo $fileInfo
-		 */
-		$ourFiles = [];
-		foreach ( new RecursiveIteratorIterator( $directoryIterator ) as $fileInfo ) {
-			if ( substr( $fileInfo->getFilename(), -8 ) === 'Test.php' ) {
-				$ourFiles[] = $fileInfo->getPathname();
-			}
-		}
-
-		$files = array_merge( $files, $ourFiles );
-		return true;
-		// @codeCoverageIgnoreEnd
-	}
-
-	/**
 	 * Makes the messenger sender exempt from IP blocks no matter what
 	 * Called only if the context is a MassMessage job (bug 69381)
 	 * @see bug 58237
