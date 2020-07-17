3f2d1ce1e00a051fefb3a59acdda4817c3c959c4
Umherirrender <umherirrender_de.wp@web.de>
2020-07-17 23:29:10 +0200

    build: Run stylelint also for less files
    Files were added with I96356147eb6abfde33fac313140af196399ee5d8
    
    Change-Id: Ia07c1e413967398e16ab52187d6647d7177bfce3

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function ( grunt ) {
 		banana: conf.MessagesDirs,
 		stylelint: {
 			all: [
-				'**/*.css',
+				'**/*.{css,less}',
 				'!node_modules/**',
 				'!vendor/**'
 			]
