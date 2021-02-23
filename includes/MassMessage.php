fa439e422e8479f2b27a54eec3ca245c11bcd008
Niklas Laxström <niklas.laxstrom@gmail.com>
2021-02-23 15:02:18 +0100

    Remove mandatory line separator between page content and message
    Bug: T254481
    Change-Id: I365f8dba78a7aa06034db42861b927b955ee1480

--- includes/MassMessage.php
+++ includes/MassMessage.php
@@ -651,7 +651,7 @@ class MassMessage {
 	 * @return string
 	 */
 	public static function appendMessageAndPage( string $message, string $pageContent ): string {
-		return $pageContent . "\n\n----\n\n" . $message;
+		return $pageContent . "\n\n" . $message;
 	}
 
 	public static function getApiEndpoint( string $wiki ): ?string {
