4b3a0d3402ac3228777de276c0dcd53b12cbd175
Brian Wolff <bawolff+wn@gmail.com>
2023-01-05 18:40:44 -0800

    Add a hook to override how a message is sent to a specific page
    This particular point in the code already has hard-coded detection
    for two specific extensions (LQT and flow). There's also an additional
    extension - MassMessageEmail which instructs users to patch this
    point of MassMessage. It seems clear that this is a code point that
    extensions need to be able to override.
    
    Also bump version since people may want to know if the hook is
    present based on the version number.
    
    Change-Id: I2de7cdbba836a0de239f4e72b0063f8a2701e83e

---
+++ includes/Job/Hooks/MassMessageJobBeforeSentHook.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MediaWiki\MassMessage\Job\Hooks;
+
+use MediaWiki\MassMessage\LanguageAwareText;
+use Title;
+
+interface MassMessageJobBeforeSentHook {
+	/**
+	 * Hook runner for `MassMessageJobBeforeMessageSent` hook
+	 *
+	 * Allows overriding how mass messages are sent. For example,
+	 * if you had a custom discussion page extension, you might want
+	 * to use this to change how messages are posted on user talk pages.
+	 *
+	 * Generally you would use the MessageBuilder class to actually
+	 * construct the text of the message.
+	 *
+	 * Returning false will prevent MassMessage from sending the message. If
+	 * your hook sent the message itself, you should return false to prevent
+	 * MassMessage from trying to send the same message again.
+	 *
+	 * @param callable $failureCallback proxy for MassMessageJob::logLocalFailure
+	 * @param Title $targetPage Page message is being sent to
+	 * @param string $subject
+	 * @param string $message
+	 * @param ?LanguageAwareText $pageSubject
+	 * @param ?LanguageAwareText $pageMessage
+	 * @param array $comment Parameters to 'massmessage-hidden-comment' message
+	 * @return bool|void True to have MassMessage send the message normally, false to not
+	 */
+	public function onMassMessageJobBeforeMessageSent(
+		callable $failureCallback,
+		Title $targetPage,
+		string $subject,
+		string $message,
+		?LanguageAwareText $pageSubject,
+		?LanguageAwareText $pageMessage,
+		array $comment
+	);
+}
