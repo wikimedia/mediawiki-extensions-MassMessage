dc1782c4c14d8ecd6ee884a9663a30230b03f2d2
sbassett <sbassett@wikimedia.org>
2024-01-17 16:30:21 -0600

    SECURITY: Use message label instead of sanitized text output for massmessage-form-page-help message
    * Follow-up bug fix to the released security patch:
      Ife6fb590af53fa0d8eb59201ce88a3c47ddde45c
    
    Bug: T347742
    Change-Id: I8a45d0d022e6e083d60423234443329d8951b4d4
    (cherry picked from commit 432912514bdb1e5605fcbf44bae6ec3d194bace7)

--- includes/Specials/SpecialMassMessage.php
+++ includes/Specials/SpecialMassMessage.php
@@ -181,7 +181,7 @@ class SpecialMassMessage extends FormSpecialPage {
 			'tabindex' => $controlTabIndex++,
 			'label-message' => 'massmessage-form-page',
 			'default' => $request->getText( 'page-message' ),
-			'help-message' => $this->msg( 'massmessage-form-page-help' )->text(),
+			'help-message' => 'massmessage-form-page-help',
 			'required' => false
 		];
 
