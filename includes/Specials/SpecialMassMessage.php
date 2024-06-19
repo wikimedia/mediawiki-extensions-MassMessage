wikimedia/mediawiki-extensions-MassMessage

commit a7e7f56cb8f369652baa8863e6d672e2b62f7142

Convert Special:MassMessage to use OOUI
Convert Special:MassMessage to use OOUI. This includes making the
Special page extend FormSpecialPage to reduce the amount of code
and also modifying the JS validation to fit with the OOUI style.
CSS and JS is also updated to reflect changing to OOUI which causes
classes and IDs to change.

The title fields were also updated to the title field type to match
with the OOUI styling as the autocomplete JS code currently used
does not match OOUI styling. One downside of this is that the
suggested pages can include pages that are not valid options.
A change to the OOUI title field is likely needed for this.

Also remove uses of $.debounce and replace it with OO.ui.debounce
as one of the two uses needed to be modified to update to OOUI.

Bug: T104419
Bug: T213426
Change-Id: Ie37fdbe04b4ae85d3f3020d943f41fbe1c477094

includes/Specials/SpecialMassMessage.php
@@ -4,6 +4,7 @@ namespace MediaWiki\MassMessage\Specials;
 
 use ContentHandler;
 use EditPage;
+use FormSpecialPage;
 use Html;
 use HTMLForm;
 use MediaWiki\MassMessage\Lookup\SpamlistLookup;
@@ -16,11 +17,13 @@ use MediaWiki\MassMessage\RequestProcessing\MassMessageRequest;
 use MediaWiki\MassMessage\RequestProcessing\MassMessageRequestParser;
 use MediaWiki\MediaWikiServices;
 use Message;
-use SpecialPage;
+use OOUI\FieldsetLayout;
+use OOUI\HtmlSnippet;
+use OOUI\PanelLayout;
+use OOUI\Widget;
 use Status;
 use Title;
 use WikiMap;
-use Xml;
 
 /**
  * Form to allow users to send messages to a lot of users at once.
@@ -29,7 +32,7 @@ use Xml;
  * @license GPL-2.0-or-later
  */
 
-class SpecialMassMessage extends SpecialPage {
+class SpecialMassMessage extends FormSpecialPage {
 	/** @var Status */
 	protected $status;
 	/** @var string */
@@ -61,16 +64,13 @@ class SpecialMassMessage extends SpecialPage {
 		return true;
 	}
 
-	/** @param string|null $par */
+	/** @inheritDoc */
 	public function execute( $par ) {
 		$request = $this->getRequest();
-		$context = $this->getContext();
 		$output = $this->getOutput();
 
 		$this->addHelpLink( 'Help:Extension:MassMessage' );
-		$this->setHeaders();
 		$this->outputHeader();
-		$this->checkPermissions();
 
 		$output->addModules( 'ext.MassMessage.special.js' );
 		$output->addModuleStyles( 'ext.MassMessage.styles' );
@@ -87,32 +87,35 @@ class SpecialMassMessage extends SpecialPage {
 			$this->state = 'form';
 		}
 
-		$form = new HTMLForm( $this->createForm(), $context );
-		$form->setId( 'mw-massmessage-form' );
-		$form->setDisplayFormat( 'div' );
-		if ( $this->state === 'form' ) {
-			$form->addPreText( $this->msg( 'massmessage-form-header' )->parse() );
-		}
-		$form->setWrapperLegendMsg( 'massmessage' );
-		$form->suppressDefaultSubmit(); // We use our own buttons.
-		$form->setSubmitCallback( [ $this, 'callback' ] );
-		$form->setMethod( 'post' );
+		parent::execute( $par );
+	}
 
-		$form->prepareForm();
-		$result = $form->tryAuthorizedSubmit();
-		if ( $result === true || ( $result instanceof Status && $result->isGood() ) ) {
-			if ( $this->state === 'submit' ) { // If it's preview, everything is shown already.
-				$output->addWikiMsg(
-					'massmessage-submitted',
-					Message::numParam( $this->count )
-				);
-				$output->addWikiMsg( 'massmessage-nextsteps' );
-			}
-		} else {
-			if ( $this->state === 'preview' ) {
-				$result = $this->status;
-			}
-			$form->displayForm( $result );
+	/** @inheritDoc */
+	protected function alterForm( HTMLForm $form ) {
+		if ( $this->state === 'form' ) {
+			$form->addPreHtml( $this->msg( 'massmessage-form-header' )->parse() );
+		}
+		return $form
+			->setId( 'mw-massmessage-form' )
+			->setWrapperLegendMsg( 'massmessage' )
+			->suppressDefaultSubmit() // We use our own buttons.
+			->setMethod( 'post' );
+	}
+
+	/** @inheritDoc */
+	protected function getDisplayFormat() {
+		return 'ooui';
+	}
+
+	/** @inheritDoc */
+	public function onSuccess() {
+		if ( $this->state === 'submit' ) {
+			$output = $this->getOutput();
+			$output->addWikiMsg(
+				'massmessage-submitted',
+				Message::numParam( $this->count )
+			);
+			$output->addWikiMsg( 'massmessage-nextsteps' );
 		}
 	}
 
@@ -135,8 +138,8 @@ class SpecialMassMessage extends SpecialPage {
 		return $this->count;
 	}
 
-	/** @return array */
-	protected function createForm() {
+	/** @inheritDoc */
+	protected function getFormFields() {
 		$request = $this->getRequest();
 		$controlTabIndex = 1;
 
@@ -147,7 +150,7 @@ class SpecialMassMessage extends SpecialPage {
 		$m['spamlist'] = [
 			'id' => 'mw-massmessage-form-spamlist',
 			'name' => 'spamlist',
-			'type' => 'text',
+			'type' => 'title',
 			'tabindex' => $controlTabIndex++,
 			'label-message' => 'massmessage-form-spamlist',
 			'default' => $request->getText( 'spamlist' )
@@ -168,11 +171,12 @@ class SpecialMassMessage extends SpecialPage {
 		$m['page-message'] = [
 			'id' => 'mw-massmessage-form-page',
 			'name' => 'page-message',
-			'type' => 'text',
+			'type' => 'title',
 			'tabindex' => $controlTabIndex++,
 			'label-message' => 'massmessage-form-page',
 			'default' => $request->getText( 'page-message' ),
-			'help' => $this->msg( 'massmessage-form-page-help' )->text()
+			'help' => $this->msg( 'massmessage-form-page-help' )->text(),
+			'required' => false
 		];
 
 		$options = [ '----' => '' ];
@@ -244,10 +248,10 @@ class SpecialMassMessage extends SpecialPage {
 	 * Does some basic verification of data.
 	 * Decides whether to show the preview screen or the submitted message.
 	 *
-	 * @param array $data
+	 * @inheritDoc
 	 * @return Status|bool
 	 */
-	public function callback( array $data ) {
+	public function onSubmit( array $data ) {
 		$requestParser = new MassMessageRequestParser();
 		$this->status = $requestParser->parseRequest( $data, $this->getUser() );
 
@@ -400,18 +404,42 @@ class SpecialMassMessage extends SpecialPage {
 		);
 		$contentRenderer = $services->getContentRenderer();
 		$parserOutput = $contentRenderer->getParserOutput( $content, $mockTarget, null, $parserOptions );
-		$previewFieldset = Xml::fieldset(
-			$this->msg( 'massmessage-fieldset-preview' )->text(),
-			$parserOutput->getText( [ 'enableSectionEditLinks' => false ] )
-		);
-		$this->getOutput()->addHTML( $previewFieldset );
+		$previewLayout = new PanelLayout( [
+			'content' => new FieldsetLayout( [
+				'label' => $this->msg( 'massmessage-fieldset-preview' )->text(),
+				'items' => [
+					new Widget( [
+						'content' => new HtmlSnippet(
+							$parserOutput->getText( [ 'enableSectionEditLinks' => false ] )
+						),
+					] ),
+				],
+			] ),
+			'expanded' => false,
+			'framed' => true,
+			'padded' => true,
+		] );
+		$this->getOutput()->addHTML( $previewLayout );
 
-		$wikitextPreviewFieldset = Xml::fieldset(
-			$this->msg( 'massmessage-fieldset-wikitext-preview' )->text(),
-			// @phan-suppress-next-line SecurityCheck-DoubleEscaped false positive or bug
-			Html::element( 'pre', [], "== {$subjectText} ==\n\n$messageText" )
-		);
-		$this->getOutput()->addHTML( $wikitextPreviewFieldset );
+		$wikitextPreviewLayout = new PanelLayout( [
+			'content' => [
+				new FieldsetLayout( [
+					'label' => $this->msg( 'massmessage-fieldset-wikitext-preview' )->text(),
+					'items' => [
+						new Widget( [
+							'content' => new HtmlSnippet(
+							// @phan-suppress-next-line SecurityCheck-DoubleEscaped false positive or bug
+								Html::element( 'pre', [], "== {$subjectText} ==\n\n$messageText" )
+							),
+						] ),
+					],
+				] ),
+			],
+			'expanded' => false,
+			'framed' => true,
+			'padded' => true,
+		] );
+		$this->getOutput()->addHTML( $wikitextPreviewLayout );
 
 		// Check if we have unescaped langlinks (Bug 54846)
 		if ( $parserOutput->getLanguageLinks() ) {
@@ -446,12 +474,21 @@ class SpecialMassMessage extends SpecialPage {
 			$infoListHtml .= '</ul>';
 		}
 
-		$infoFieldset = Xml::fieldset(
-			$this->msg( 'massmessage-fieldset-info' )->text(),
-			$infoListHtml
-		);
+		$infoLayout = new PanelLayout( [
+			'content' => new FieldsetLayout( [
+				'label' => $this->msg( 'massmessage-fieldset-info' )->text(),
+				'items' => [
+					new Widget( [
+						'content' => new HtmlSnippet( $infoListHtml )
+					] ),
+				],
+			] ),
+			'expanded' => false,
+			'framed' => true,
+			'padded' => true,
+		] );
 
-		$this->getOutput()->addHTML( $infoFieldset );
+		$this->getOutput()->addHTML( $infoLayout );
 	}
 
 	/**

modules/ext.MassMessage.badhtml.js
@@ -23,7 +23,7 @@ function badHtml( $msg ) {
 	$msg.after( $warnings );
 	$warnings.hide();
 
-	$msg.on( 'keyup', $.debounce( 500, function () {
+	$msg.on( 'keyup', OO.ui.debounce( function () {
 		var code, matches, tags, results, tagName;
 
 		code = $msg.val().trim();
@@ -79,7 +79,7 @@ function badHtml( $msg ) {
 		} else {
 			$warnings.hide();
 		}
-	} ) );
+	}, 500 ) );
 }
 
 module.exports = badHtml;

modules/ext.MassMessage.special.js
@@ -1,16 +1,12 @@
 $( function () {
 	'use strict';
 
-	// Dynamic page title validation
-	var autocomplete = require( './ext.MassMessage.autocomplete.js' ),
-		badHtml = require( './ext.MassMessage.badhtml.js' ),
-		$spamlist = $( '#mw-massmessage-form-spamlist' ),
-		$massmessagepage = $( '#mw-massmessage-form-page' );
+	var badHtml = require( './ext.MassMessage.badhtml.js' );
 
 	// Limit edit summaries to 240 bytes
 	$( '#mw-massmessage-form-subject' ).byteLimit();
 
-	badHtml( $( '#mw-massmessage-form-message' ) );
+	badHtml( $( '#mw-massmessage-form-message textarea' ) );
 
 	/**
 	 * Fetch pages with a given title.
@@ -35,50 +31,87 @@ $( function () {
 	 * Adds a status field for the element.
 	 *
 	 * @param {Object} $elem jQuery element for which the status field has to be added.
-	 * @return {Object} jQuery element
+	 * @return {Object[]} Array of the jQuery element for the OOUI error message widget
+	 *  and of the OOUI label inside the widget.
 	 */
 	function addStatusField( $elem ) {
-		var $statusField = $( '<span>' )
-			.prop( 'id', $elem.prop( 'id' ) + '-status' )
-			.insertAfter( $elem );
+		var message = new OO.ui.MessageWidget( {
+			icon: 'error',
+			type: 'error',
+			inline: true,
+			classes: [ 'mw-massmessage-form-error' ]
+		} );
+		message.$element.hide();
+		$( $elem ).closest( '.mw-htmlform-field-HTMLTitleTextField' ).append( message.$element );
+		return [ message.$element, message.$label ];
+	}
 
-		return $statusField;
+	/**
+	 * Validate the title in the form input field given
+	 * in $elem. If the field is not valid, the function
+	 * will show an appropriate error message under the
+	 * input.
+	 *
+	 * @param $elem The jQuery element for the OOUI title field to be validated
+	 * @param callback A callback function which is provided the list of pages by the title
+	 *  and which returns a boolean value of whether the title is valid (true for valid).
+	 * @param $statusField The OOUI error message for the field which may or may not be hidden
+	 * @param $statusFieldLabel The label for the OOUI error message in $statusField
+	 */
+	function validateTitle( $elem, callback, $statusField, $statusFieldLabel ) {
+		var pagetitle = $( 'input', $elem ).val();
+
+		if ( !pagetitle ) {
+			$statusField.hide();
+			return;
+		}
+
+		getPagesByTitle( pagetitle ).done( function ( data ) {
+			var result = false;
+			if ( data && data.query && !data.query.pages[ 0 ].missing ) {
+				result = callback( data.query.pages );
+			}
+
+			if ( result ) {
+				$( $elem ).removeClass( 'oo-ui-flaggedElement-invalid' );
+				$statusField.hide();
+			} else {
+				$( $elem ).addClass( 'oo-ui-flaggedElement-invalid' );
+				if ( $elem.prop( 'id' ) === 'mw-massmessage-form-spamlist' ) {
+					$( $statusFieldLabel ).text(
+						mw.message( 'massmessage-parse-badspamlist', pagetitle ).text()
+					);
+				} else {
+					$( $statusFieldLabel ).text(
+						mw.message( 'massmessage-parse-badpage', pagetitle ).text()
+					);
+				}
+				$statusField.show();
+			}
+		} );
 	}
 
 	/**
 	 * Adds page title validation for a given text field
 	 *
 	 * @param {Object} $elem jQuery element to
-	 * @param {Function} callback Called when we recieve some pages in response.
+	 * @param {Function} callback Called when we receive some pages in response.
 	 */
 	function addPageTitleValidation( $elem, callback ) {
-		var $statusField = addStatusField( $elem );
-		$elem.on( 'input autocompletechange', $.debounce( 250, function () {
-			var pagetitle = $elem.val();
-			if ( !pagetitle ) {
-				$statusField
-					.removeClass( 'invalid' )
-					.text( '' );
-				return;
-			}
-
-			getPagesByTitle( pagetitle ).done( function ( data ) {
-				var result = false;
-				if ( data && data.query && !data.query.pages[ 0 ].missing ) {
-					result = callback( data.query.pages );
-				}
-
-				if ( result ) {
-					$statusField
-						.removeClass( 'invalid' )
-						.text( '' );
-				} else {
-					$statusField
-						.addClass( 'invalid' )
-						.text( mw.message( 'massmessage-parse-badpage', pagetitle ).text() );
-				}
-			} );
-		} ) );
+		var $result = addStatusField( $elem );
+		var $statusField = $result[ 0 ];
+		var $statusFieldLabel = $result[ 1 ];
+		validateTitle( $elem, callback, $statusField, $statusFieldLabel );
+		var widget = OO.ui.infuse( $( $elem ) );
+		widget.on(
+			'change',
+			OO.ui.debounce(
+				function () {
+					validateTitle( $elem, callback, $statusField, $statusFieldLabel );
+				},
+				250
+			)
+		);
 	}
 
 	function isValidSpamList( pages ) {
@@ -92,17 +125,11 @@ $( function () {
 
 	// Only bind once for 'blur' so that the user can fill it in without errors;
 	// after that, look at every change for immediate feedback.
-	$spamlist.one( 'blur', function () {
-		addPageTitleValidation( $( this ), isValidSpamList );
+	$( $( '#mw-massmessage-form-spamlist input' ) ).one( 'blur', function () {
+		addPageTitleValidation( $( '#mw-massmessage-form-spamlist' ), isValidSpamList );
 	} );
 
-	$massmessagepage.one( 'blur', function () {
-		addPageTitleValidation( $( this ), isValidPageMessage );
+	$( $( '#mw-massmessage-form-page input' ) ).one( 'blur', function () {
+		addPageTitleValidation( $( '#mw-massmessage-form-page' ), isValidPageMessage );
 	} );
-
-	// Autocomplete for spamlist titles
-	autocomplete.enableTitleComplete( $spamlist );
-
-	// Autocomplete for pages to send as message
-	autocomplete.enableTitleComplete( $massmessagepage );
 } );

modules/ext.MassMessage.styles/special.less
@@ -3,39 +3,23 @@
  * pages, where they shouldn't do anything. Limit the scope to the special page
  */
 
-.mw-special-MassMessage {
-	.mw-body-content fieldset {
-		margin: 1em 0 1em 0;
+.mw-special-MassMessage #mw-massmessage-form {
+	.mw-massmessage-form-error {
+		margin-top: 8px;
+		margin-left: 12px;
+		margin-right: 12px;
+		min-height: 20px;
 	}
 
-	#mw-massmessage-form {
-		.mw-htmlform-field-HTMLTextField,
-		.mw-htmlform-field-HTMLTextAreaField,
-		.mw-htmlform-field-HTMLSelectField {
-			margin-bottom: 0.7em;
+	.mw-htmlform-field-HTMLSubmitField {
+		&,
+		.mw-label,
+		.mw-input {
+			display: inline-block;
 		}
 
-		input,
-		textarea {
-			margin: 0.5em 0;
-		}
-
-		.htmlform-tip {
-			font-size: 0.95em;
-			font-style: italic;
-			margin-top: -0.7em;
-			margin-bottom: 0.7em;
-		}
-
-		/*
-		 Formats the Save/Preview buttons to show up on the same line
-		*/
-		.mw-htmlform-field-HTMLSubmitField {
-			&,
-			.mw-label,
-			.mw-input {
-				display: inline;
-			}
+		#mw-massmessage-form-submit-button {
+			margin-right: 5px;
 		}
 	}
 }

commit ee3fc829ae17f5a7543a95995321d5b0eda464ac

Fix displaying check failures while previewing
A regression introduced in a7e7f56.

Change-Id: I3c9772392c4d032676905d6de9886561fcdf7564

includes/Specials/SpecialMassMessage.php
@@ -290,6 +290,13 @@ class SpecialMassMessage extends FormSpecialPage {
 		} else {
 			// $this->state can only be 'preview' here
 			$this->preview( $this->status->getValue() );
+
+			// Die on errors.
+			if ( !$this->status->isOK() ) {
+				$this->state = 'form';
+				return $this->status;
+			}
+
 			// No submission attempted
 			return false;
 		}
@@ -467,12 +474,12 @@ class SpecialMassMessage extends FormSpecialPage {
 		] );
 		$this->getOutput()->addHTML( $wikitextPreviewLayout );
 
-		// Check if we have unescaped langlinks (Bug 54846)
+		// Check if we have unescaped langlinks (T56846)
 		if ( $parserOutput->getLanguageLinks() ) {
 			$this->status->fatal( 'massmessage-unescaped-langlinks' );
 		}
 
-		// Check for unclosed HTML tags (Bug 54909)
+		// Check for unclosed HTML tags (T56909)
 		$unclosedTags = $this->getUnclosedTags( $request->getMessage() );
 		if ( $unclosedTags ) {
 			$this->status->fatal(
@@ -486,7 +493,7 @@ class SpecialMassMessage extends FormSpecialPage {
 
 		/** @var TextContent $content */
 		'@phan-var TextContent $content';
-		// Check for no timestamp (Bug 54848)
+		// Check for no timestamp (T56848)
 		if ( !preg_match( MassMessage::getTimestampRegex(), $content->getText() ) ) {
 			$this->status->fatal( 'massmessage-no-timestamp' );
 		}