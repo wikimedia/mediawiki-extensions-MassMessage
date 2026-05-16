136ea01b5921f4661c23c4be4c0c17a678954cbc
Umherirrender <umherirrender_de.wp@web.de>
2026-05-16 10:56:22 +0200

    tests: Inline Stub builder classes in LocalMessageContentFetcherTest
    The builder classes extends PHPUnit TestCase,
    but the constructor is internal to PHPUnit and changed with PHPUnit 10.
    To make this extension work on PHPUnit the builder classes must be
    removed, so inline the code as it is used only in one test.
    
    Change-Id: I277f9f3f6695ff1ec133c1e52b917a993429c823

--- tests/phpunit/messagecontentfetcher/LocalMessageContentFetcherTest.php
+++ tests/phpunit/messagecontentfetcher/LocalMessageContentFetcherTest.php
@@ -3,9 +3,12 @@ declare( strict_types = 1 );
 
 namespace MediaWiki\MassMessage\MessageContentFetcher;
 
+use MediaWiki\Content\TextContent;
+use MediaWiki\Language\Language;
 use MediaWiki\MassMessage\LanguageAwareText;
-use MediaWiki\MassMessage\Stub\RevisionStoreStubFactory;
-use MediaWiki\MassMessage\Stub\TitleStubFactory;
+use MediaWiki\Revision\RevisionRecord;
+use MediaWiki\Revision\RevisionStore;
+use MediaWiki\Title\Title;
 use MediaWikiIntegrationTestCase;
 
 class LocalMessageContentFetcherTest extends MediaWikiIntegrationTestCase {
@@ -13,8 +16,8 @@ class LocalMessageContentFetcherTest extends MediaWikiIntegrationTestCase {
 	 * @covers \MediaWiki\MassMessage\MessageContentFetcher\LocalMessageContentFetcher::getContent
 	 */
 	public function testGetContentTitleNotFound() {
-		$revisionStoreStub = ( new RevisionStoreStubFactory() )->getWithoutRevisionRecord();
-		$titleStub = ( new TitleStubFactory() )->getNonExistingTitle( 'not-found', 'en', 'ltr' );
+		$revisionStoreStub = $this->getRevisionStoreWithoutRevisionRecord();
+		$titleStub = $this->getNonExistingTitle( 'not-found', 'en', 'ltr' );
 
 		$localContentFetcher = new LocalMessageContentFetcher( $revisionStoreStub );
 		$status = $localContentFetcher->getContent( $titleStub );
@@ -24,12 +27,61 @@ class LocalMessageContentFetcherTest extends MediaWikiIntegrationTestCase {
 		);
 	}
 
+	private function getRevisionStoreWithText( string $textContent ): RevisionStore {
+		$revisionRecordStub = $this->createStub( RevisionRecord::class );
+		$revisionRecordStub->method( 'getContent' )
+			->willReturn( new TextContent( $textContent ) );
+
+		$revisionStoreStub = $this->createStub( RevisionStore::class );
+		$revisionStoreStub->method( 'getRevisionByTitle' )
+			->willReturn( $revisionRecordStub );
+
+		return $revisionStoreStub;
+	}
+
+	private function getRevisionStoreWithoutRevisionRecord(): RevisionStore {
+		$revisionRecordStub = $this->createStub( RevisionStore::class );
+		$revisionRecordStub->method( 'getRevisionByTitle' )
+			->willReturn( null );
+
+		return $revisionRecordStub;
+	}
+
+	private function getExistingTitle( string $titleStr, string $languageCode, string $languageDir ): Title {
+		return $this->getTitleStub( $titleStr, true, $languageCode, $languageDir );
+	}
+
+	private function getNonExistingTitle( string $titleStr, string $languageCode, string $languageDir ): Title {
+		return $this->getTitleStub( $titleStr, false, $languageCode, $languageDir );
+	}
+
+	private function getTitleStub( string $titleStr, bool $exists, string $languageCode, string $languageDir ): Title {
+		$titleStub = $this->createStub( Title::class );
+		$titleStub->method( 'exists' )
+			->willReturn( $exists );
+
+		$titleStub->method( 'getPrefixedText' )
+			->willReturn( $titleStr );
+
+		$languageStub = $this->createStub( Language::class );
+		$languageStub->method( 'getCode' )
+			->willReturn( $languageCode );
+
+		$languageStub->method( 'getDir' )
+			->willReturn( $languageDir );
+
+		$titleStub->method( 'getPageLanguage' )
+			->willReturn( $languageStub );
+
+		return $titleStub;
+	}
+
 	/**
 	 * @covers \MediaWiki\MassMessage\MessageContentFetcher\LocalMessageContentFetcher::getContent
 	 */
 	public function testGetContentWithoutRevisionRecord() {
-		$revisionStoreStub = ( new RevisionStoreStubFactory() )->getWithoutRevisionRecord();
-		$titleStub = ( new TitleStubFactory() )->getExistingTitle( 'valid', 'en', 'ltr' );
+		$revisionStoreStub = $this->getRevisionStoreWithoutRevisionRecord();
+		$titleStub = $this->getExistingTitle( 'valid', 'en', 'ltr' );
 
 		$localContentFetcher = new LocalMessageContentFetcher( $revisionStoreStub );
 		$status = $localContentFetcher->getContent( $titleStub );
@@ -47,10 +99,10 @@ class LocalMessageContentFetcherTest extends MediaWikiIntegrationTestCase {
 		$contentLanguage = 'he';
 		$contentDir = 'rtl';
 
-		$revisionStoreStub = ( new RevisionStoreStubFactory() )->getWithText( $contentText );
+		$revisionStoreStub = $this->getRevisionStoreWithText( $contentText );
 		$localContentFetcher = new LocalMessageContentFetcher( $revisionStoreStub );
 
-		$titleStub = ( new TitleStubFactory() )->getExistingTitle( 'valid', $contentLanguage, $contentDir );
+		$titleStub = $this->getExistingTitle( 'valid', $contentLanguage, $contentDir );
 		$status = $localContentFetcher->getContent( $titleStub );
 		$this->assertStatusOK( $status );
 
